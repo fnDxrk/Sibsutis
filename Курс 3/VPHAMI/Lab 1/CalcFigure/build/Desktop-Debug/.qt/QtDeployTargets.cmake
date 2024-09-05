02a5001affcd3f0fcb7f861dacd298f54ad37114
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-09-05 22:06:36 +0700

    VPHAMI start Lab 1

---
+++ Курс 3/VPHAMI/Lab 1/CalcFigure/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_CalcFigure_FILE /home/dxrk_/Documents/Sibsutis/Курс 3/VPHAMI/Lab 1/CalcFigure/build/Desktop-Debug/CalcFigure)
+set(__QT_DEPLOY_TARGET_CalcFigure_TYPE EXECUTABLE)
---
+++ Курс 3/VPHAMI/Lab 1/CalcFigure/build/Desktop-Debug/CMakeFiles/CalcFigure.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalcFigure_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/CalcFigure_autogen.dir/ParseCache.txt"
+  "CalcFigure_autogen"
+  "CMakeFiles/CalcFigure.dir/CalcFigure_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/CalcFigure.dir/CalcFigure_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/CalcFigure.dir/main.cpp.o"
+  "CMakeFiles/CalcFigure.dir/main.cpp.o.d"
+  "CMakeFiles/CalcFigure.dir/mainwindow.cpp.o"
+  "CMakeFiles/CalcFigure.dir/mainwindow.cpp.o.d"
+  "CalcFigure"
+  "CalcFigure.pdb"
+  "CalcFigure_autogen/mocs_compilation.cpp"
+  "CalcFigure_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalcFigure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
