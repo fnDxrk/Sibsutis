31cd2a3b57033faacc29e1b79659bcd9f0df7cbe
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-09-05 22:01:29 +0700

    AVS Lab 1

---
+++ Курс 3/AVS/Lab 1/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "OS          $(lsb_release -sd | tr -d '"')"
+echo -e "Kernel      $(uname -srm)\n"
+
+echo -e "CPU         $(lscpu | grep "Имя модели" | cut -d ':' -f2 | xargs)"
+echo -e "CPU MHz     $(cat /proc/cpuinfo | grep "Hz" | cut -d ':' -f2 | head -n 1 | xargs)"
+echo -e "CPU Core    $(lscpu | grep "Ядер" | cut -d ':' -f2 | head -n 1 | xargs)"
+echo -e "CPU Cache   $(cat /proc/cpuinfo | grep "cache" | cut -d ':' -f2 | head -n 1 | xargs)\n"
+
+echo -e "Mem free    $(free --mega | grep Mem | tr -s ' ' | cut -d " " -f4) MB"
+echo -e "Mem total   $(free --mega | grep Mem | tr -s ' ' | cut -d " " -f2) MB"
+echo -e "Mem used    $(free --mega | grep Mem | tr -s ' ' | cut -d " " -f3) MB\n"
+
+echo -e "User        $(whoami)"
+echo -e "IP          $(ip -br a show | grep UP | tr -s ' ' | cut -d " " -f3)"
+echo -e "MAC address $(ip a | grep ether | tail -n 1 | tr -s ' ' | cut -d " " -f3)\n"
+
+echo -e "$(df -h | grep ^/dev/ | awk '{printf "%-10s %-10s %s/%s\n", $6, $2, $3, $4}')"
+
