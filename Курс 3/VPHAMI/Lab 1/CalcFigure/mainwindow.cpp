4721da96eb95a848cab913d2ee390e23391fc00c
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-09-07 15:57:21 +0700

    UI code

--- Курс 3/VPHAMI/Lab 1/CalcFigure/mainwindow.cpp
+++ Курс 3/VPHAMI/Lab 1/CalcFigure/mainwindow.cpp
@@ -32,12 +32,69 @@ void MainWindow::on_menu_3_triggered()
     QApplication::quit();
 }
 
-// Функция установки изображения по индексу выбранной фигуры
-void MainWindow::on_comboBox_activated(int index)
+void MainWindow::argumentUI(const QString &text1, bool show2 = false, const QString &text2 = "", bool show3 = false, const QString &text3 = "") {
+    ui->text_1->setText(text1);
+
+    if (show2) {
+        ui->text_2->setText(text2);
+        ui->text_2->show();
+        ui->line_2->show();
+    } else {
+        ui->text_2->hide();
+        ui->line_2->hide();
+    }
+
+    if (show3) {
+        ui->text_3->setText(text3);
+        ui->text_3->show();
+        ui->line_3->show();
+    } else {
+        ui->text_3->hide();
+        ui->line_3->hide();
+    }
+}
+
+void MainWindow::updateUI(int index)
+{
+    switch (index) {
+    case 0:
+        argumentUI("Длина");
+        break;
+    case 1:
+        argumentUI("Длина", true, "Высота");
+        break;
+    case 2:
+        argumentUI("Основание", true, "Высота");
+        break;
+    case 3:
+        argumentUI("Диагональ 1", true, "Диагональ 2");
+        break;
+    case 4:
+        argumentUI("Основание", true, "Высота");
+        break;
+    case 5:
+        argumentUI("Основание 1", true, "Основание 2", true, "Высота");
+        break;
+    case 6:
+        argumentUI("Радиус");
+        break;
+    case 7:
+        argumentUI("Радиус", true, "Угол");
+        break;
+    }
+}
+
+void MainWindow::setImage(int index)
 {
     if (index >= 0 ) {
         ui->labelImage->setStyleSheet("image : url(" + images[index] + ")");
     }
+}
+
+void MainWindow::on_comboBox_activated(int index)
+{
+    updateUI(index);
+    setImage(index);
 }
 
 void MainWindow::on_pushButton_clicked()
--- Курс 3/VPHAMI/Lab 1/CalcFigure/mainwindow.h
+++ Курс 3/VPHAMI/Lab 1/CalcFigure/mainwindow.h
@@ -24,9 +24,18 @@ private slots:
 
     void on_comboBox_activated(int index);
 
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
     // Массив изображений фигур
     QStringList images;
+    // Функция для опредения видимости интерфейса
+    void argumentUI(const QString &text1, bool show2, const QString &text2, bool show3, const QString &text3);
+    // Функция для обновления интерфейса
+    void updateUI(int index);
+    // Функция для выбора изображения при изменении индекса
+    void setImage(int index);
 };
 #endif // MAINWINDOW_H
