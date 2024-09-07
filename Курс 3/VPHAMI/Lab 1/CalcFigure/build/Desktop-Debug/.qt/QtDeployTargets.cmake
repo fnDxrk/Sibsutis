5ff043ac50b753f2eaee379e0c5a17df6ca76ae4
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-09-07 15:59:12 +0700

    Remove folder

--- Курс 3/VPHAMI/Lab 1/CalcFigure/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_CalcFigure_FILE /home/dxrk_/Documents/Sibsutis/Курс 3/VPHAMI/Lab 1/CalcFigure/build/Desktop-Debug/CalcFigure)
-set(__QT_DEPLOY_TARGET_CalcFigure_TYPE EXECUTABLE)
