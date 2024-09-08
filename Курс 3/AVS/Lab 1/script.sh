a43ddb91cfd810261dd07986963cc14f7a63bd05
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-09-08 15:27:55 +0700

    Replace with xargs

--- Курс 3/AVS/Lab 1/script.sh
+++ Курс 3/AVS/Lab 1/script.sh
@@ -8,13 +8,13 @@ echo -e "CPU MHz     $(cat /proc/cpuinfo | grep "Hz" | cut -d ':' -f2 | head -n 
 echo -e "CPU Core    $(lscpu | grep "Ядер" | cut -d ':' -f2 | head -n 1 | xargs)"
 echo -e "CPU Cache   $(cat /proc/cpuinfo | grep "cache" | cut -d ':' -f2 | head -n 1 | xargs)\n"
 
-echo -e "Mem free    $(free --mega | grep Mem | tr -s ' ' | cut -d " " -f4) MB"
-echo -e "Mem total   $(free --mega | grep Mem | tr -s ' ' | cut -d " " -f2) MB"
-echo -e "Mem used    $(free --mega | grep Mem | tr -s ' ' | cut -d " " -f3) MB\n"
+echo -e "Mem free    $(free --mega | grep Mem | xargs | cut -d " " -f4) MB"
+echo -e "Mem total   $(free --mega | grep Mem | xargs | cut -d " " -f2) MB"
+echo -e "Mem used    $(free --mega | grep Mem | xargs | cut -d " " -f3) MB\n"
 
 echo -e "User        $(whoami)"
-echo -e "IP          $(ip -br a show | grep UP | tr -s ' ' | cut -d " " -f3)"
-echo -e "MAC address $(ip a | grep ether | tail -n 1 | tr -s ' ' | cut -d " " -f3)\n"
+echo -e "IP          $(ip -br a show | grep UP | xargs | cut -d " " -f3)"
+echo -e "MAC address $(ip a | grep ether | tail -n 1 | xargs | cut -d " " -f3)\n"
 
 echo -e "$(df -h | grep ^/dev/ | awk '{printf "%-10s %-10s %s/%s\n", $6, $2, $3, $4}')"
 
