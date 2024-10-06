0a38390350c01d8eeab453acf4e670197a2a007c
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-10-07 00:07:17 +0700

    Define the primary device

--- Курс 3/OS/Coursework/src/main.cpp
+++ Курс 3/OS/Coursework/src/main.cpp
@@ -1,9 +1,41 @@
+#include "PcapLiveDeviceList.h"
+#include "SystemUtils.h"
+#include "stdlib.h"
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
 #include <ostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    auto devices = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDevicesList();
 
-    return 0;
+    if (devices.empty()) {
+        std::cerr << "No devices found." << std::endl;
+        return 1;
+    }
+
+    // List devices
+    std::cout << "List devices :" << std::endl;
+    std::size_t index = 0;
+    for (const auto& dev : devices) {
+        std::cout << index + 1 << ". Name : " << dev->getName() << std::endl;
+        index++;
+    }
+    std::cout << std::endl;
+
+    std::cout << "Enter the number of the selected device : ";
+    int number_device;
+    std::cin >> number_device;
+
+    if (std::cin.fail() || number_device < 1 || number_device > devices.size()) {
+        std::cerr << "Invalid selection. Please enter a number between 1 and "
+                  << devices.size() << ".\n";
+        return 1;
+    }
+
+    std::system("clear");
+
+    // Main device
+    auto selectedDevice = devices[number_device - 1];
 }
