9ad3d32b1eef4bfe874804469c65880ea2f87e75
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-11-06 22:14:13 +0700

    FLP Refactor Lab_5_5

--- Курс 3/FLP/Lab 5/lab5_4.pl
+++ Курс 3/FLP/Lab 5/lab5_4.pl
@@ -1,24 +1,16 @@
-% Основной предикат
 most_frequent_elements :-
-    % Запрашиваем у пользователя список
     write('Введите список: '),
     read(List),
-    % Подсчитываем частоту каждого элемента
     count_frequencies(List, [], FrequencyList),
-    % Определяем максимальную частоту
     max_frequency(FrequencyList, MaxFreq),
-    % Находим элементы с максимальной частотой
     collect_most_frequent(FrequencyList, MaxFreq, MostFrequent),
-    % Выводим результат
     format('Наиболее часто встречающиеся элементы: ~w~n', [MostFrequent]).
 
-% Подсчет количества вхождений каждого элемента в список пар (элемент-частота)
 count_frequencies([], Acc, Acc).
 count_frequencies([H|T], Acc, FrequencyList) :-
     update_frequency(H, Acc, UpdatedAcc),
     count_frequencies(T, UpdatedAcc, FrequencyList).
 
-% Обновление списка частот: либо добавляем новый элемент, либо увеличиваем частоту
 update_frequency(Element, [], [(Element, 1)]).
 update_frequency(Element, [(Element, Freq)|T], [(Element, NewFreq)|T]) :-
     NewFreq is Freq + 1.
@@ -26,13 +18,11 @@ update_frequency(Element, [(OtherElement, Freq)|T], [(OtherElement, Freq)|Update
     Element \= OtherElement,
     update_frequency(Element, T, UpdatedT).
 
-% Нахождение максимальной частоты
 max_frequency([( _, Freq)], Freq).
 max_frequency([(_, Freq)|T], MaxFreq) :-
     max_frequency(T, TailMaxFreq),
     MaxFreq is max(Freq, TailMaxFreq).
 
-% Сбор элементов с максимальной частотой
 collect_most_frequent([], _, []).
 collect_most_frequent([(Element, Freq)|T], MaxFreq, [Element|MostFrequent]) :-
     Freq =:= MaxFreq,
