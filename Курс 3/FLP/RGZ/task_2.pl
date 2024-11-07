ae5a6871a49dfc65ad1bd45cabb4602468dac2b1
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-11-07 11:37:09 +0700

    FLP RGZ Refactor task_2

--- Курс 3/FLP/RGZ/task_2.pl
+++ Курс 3/FLP/RGZ/task_2.pl
@@ -1,7 +1,9 @@
 :- use_module(library(lists)).
-:- use_module(library(apply)).
 
-process_text_file(InputFile, OutputFile) :-
+task_2(InputFile, OutputFile) :-
+    process_file(InputFile, OutputFile).
+
+process_file(InputFile, OutputFile) :-
     open(InputFile, read, InStream),
     open(OutputFile, write, OutStream),
     process_lines(InStream, OutStream),
@@ -9,50 +11,43 @@ process_text_file(InputFile, OutputFile) :-
     close(OutStream).
 
 process_lines(InStream, OutStream) :-
-    read_line_to_codes(InStream, Line),
-    ( Line == end_of_file
+    read_line_to_string(InStream, CurrentLine),
+    ( CurrentLine == end_of_file
     -> true
-    ;  atom_codes(Atom, Line),
-       split_string(Atom, " ", "", Words),
-       format_line(Words, FormattedLine),
-       format(OutStream, '~s~n', [FormattedLine]),
+    ;  split_string(CurrentLine, " ", "", WordsList),
+       justify_words(WordsList, JustifiedLine),
+       writeln(OutStream, JustifiedLine),
        process_lines(InStream, OutStream)
     ).
 
-format_line(Words, FormattedLine) :-
-    length(Words, NumWords),
-    NumWords > 1,
-    maplist(string_length, Words, WordLengths),
+justify_words(WordsList, JustifiedLine) :-
+    length(WordsList, WordCount),
+    WordCount > 1,                           
+    maplist(string_length, WordsList, WordLengths),
     sum_list(WordLengths, TotalWordLength),
-    SpacesNeeded is 80 - TotalWordLength,
-    NumGaps is NumWords - 1,
-    ExtraSpaces is SpacesNeeded mod NumGaps,
-    BaseSpaces is SpacesNeeded // NumGaps,
-    distribute_spaces(Words, BaseSpaces, ExtraSpaces, FormattedLine).
+    RequiredSpaces is 80 - TotalWordLength,
+    NumberOfGaps is WordCount - 1,
+    BaseSpaces is RequiredSpaces // NumberOfGaps,
+    ExtraSpaces is RequiredSpaces mod NumberOfGaps,
+    distribute_spaces(WordsList, BaseSpaces, ExtraSpaces, JustifiedLine).
 
-distribute_spaces([Word], _, _, Word).
-distribute_spaces([Word1, Word2 | Words], BaseSpaces, ExtraSpaces, FormattedLine) :-
+distribute_spaces([Word], _, _, Word).     
+distribute_spaces([FirstWord, SecondWord | RemainingWords], BaseSpaces, ExtraSpaces, JustifiedLine) :-
     ( ExtraSpaces > 0
-    -> Spaces is BaseSpaces + 1, NewExtraSpaces is ExtraSpaces - 1
-    ;  Spaces is BaseSpaces, NewExtraSpaces is ExtraSpaces
+    -> ExtraSpacesForThisGap is BaseSpaces + 1,
+       NewExtraSpaces is ExtraSpaces - 1
+    ;  ExtraSpacesForThisGap is BaseSpaces,
+       NewExtraSpaces is ExtraSpaces
     ),
-    generate_spaces(Spaces, SpacesAtom),
-    atom_concat(Word1, SpacesAtom, Temp),
-    distribute_spaces([Word2 | Words], BaseSpaces, NewExtraSpaces, Rest),
-    atom_concat(Temp, Rest, FormattedLine).
+    create_spaces(ExtraSpacesForThisGap, SpaceString),
+    string_concat(FirstWord, SpaceString, TempLine),
+    distribute_spaces([SecondWord | RemainingWords], BaseSpaces, NewExtraSpaces, RestLine),
+    string_concat(TempLine, RestLine, JustifiedLine).
 
-generate_spaces(0, "").
-generate_spaces(N, Spaces) :-
-    N > 0,
-    N1 is N - 1,
-    generate_spaces(N1, Spaces1),
-    atom_concat(" ", Spaces1, Spaces).
+create_spaces(0, "").
+create_spaces(Number, Spaces) :-
+    Number > 0,
+    NextNumber is Number - 1,
+    create_spaces(NextNumber, SubSpaces),
+    string_concat(" ", SubSpaces, Spaces).
 
-main :-
-    write('Input file name: '),
-    read_line_to_codes(user_input, InputCodes),
-    atom_codes(InputFile, InputCodes),
-    write('Output file name: '),
-    read_line_to_codes(user_input, OutputCodes),
-    atom_codes(OutputFile, OutputCodes),
-    process_text_file(InputFile, OutputFile).
