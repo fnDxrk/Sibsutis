0825a516722aea605384aad0bac044d4ae99e74a
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-11-07 11:43:35 +0700

    FLP RGZ Refactor task_1

--- Курс 3/FLP/RGZ/task_1.pl
+++ Курс 3/FLP/RGZ/task_1.pl
@@ -1,10 +1,10 @@
-double_elements([], []).
-double_elements([Head|Tail], [Head,Head|DoubledTail]) :-
-    double_elements(Tail, DoubledTail).
-
 task_1 :-
     write('Введите список : '),
     read(InputList),
     double_elements(InputList, DoubledList),
     write('Удвоенный список : '),
     format('Удвоенный список : ~w\n', [DoubledList]).
+
+double_elements([], []).
+double_elements([Head|Tail], [Head,Head|DoubledTail]) :-
+    double_elements(Tail, DoubledTail).
