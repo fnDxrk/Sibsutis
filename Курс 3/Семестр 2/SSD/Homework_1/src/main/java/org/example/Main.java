4a8926932a6d04eb75a66c22cda82c4284183e74
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-02-06 23:09:09 +0700

    Add support for command-line options

--- Курс 3/Семестр 2/SSD/Homework_1/src/main/java/org/example/Main.java
+++ Курс 3/Семестр 2/SSD/Homework_1/src/main/java/org/example/Main.java
@@ -1,17 +1,54 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how GIGA IDE suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String outputPath = ".";
+        String prefix = "";
+        boolean appendEnabled = false;
+        StatsMode statsMode = StatsMode.NONE;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < args.length; i++) {
+            switch(args[i]) {
+                case "-o" -> {
+                    if (i + 1 < args.length) {
+                        outputPath = args[++i];
+                    } else {
+                        System.err.println("Ошибка! Отсутствует путь после параметра -o");
+                        System.exit(1);
+                    }
+                }
+                case "-p" -> {
+                    if (i + 1 < args.length) {
+                        prefix = args[++i];
+                    } else {
+                        System.err.println("Ошибка! Отсутствует префикс после параметра -p");
+                        System.exit(1);
+                    }
+                }
+                case "-a"-> appendEnabled = true;
+                case "-s" -> statsMode = StatsMode.SHORT;
+                case "-f" -> statsMode = StatsMode.FULL;
+                default -> {
+                    System.err.println("Ошибка! Указан неверный параметр: " + args[i]);
+                    System.exit(1);
+                }
+            }
         }
+
+        String integersFiles = outputPath + File.separator + prefix + "integers.txt";
+        String floatsFiles = outputPath + File.separator + prefix + "floats.txt";
+        String stringsFiles = outputPath + File.separator +  prefix + "strings.txt";
+
+        System.out.println("Путь для результатов: " + outputPath);
+        System.out.println("Префикс: " + prefix);
+        System.out.println("Режим добавления: " + appendEnabled);
+        System.out.println("Режим статистики: " + statsMode);
+        System.out.println("Полный путь до файла с числами: " + integersFiles);
     }
+}
+
+enum StatsMode {
+    NONE, SHORT, FULL
 }
