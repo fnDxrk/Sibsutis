f72f90b561e0ae8a3891fe415e8cca87d1f1c55a
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-10-28 12:10:10 +0700

    DB: Lab 2 complete

--- Курс 4/Семестр 1/DB/lab2.sql
+++ Курс 4/Семестр 1/DB/lab2.sql
@@ -1,46 +1,37 @@
 set search_path to my_schema;
 
 -- 1 --
-
 select name from prod
 where name like 's%s';
 
 -- 2 --
-
-select distinct city from cust 
-where city like '%r_' 
+select distinct city from cust
+where city like '%r_'
 order by city;
 
 -- 3 --
-
-select lower(substring(name from 2)) as m_name from sal
+select lower(substring(name from 2)) as name from sal
 where length(name) > 5
-order by m_name;
+order by name;
 
 -- 4 --
-
 select lower(name) || ': ' || rating || ' scores' as result from cust
-where position('ev' in name) > 0;
+where name like '%ev%';
 
 -- 5 --
-
-select distinct to_char(ord_date, 'DD') as day,
-to_char(ord_date, 'MM') as month,
-to_char(ord_date, 'YY') as year from ord
-order by year, month, day;
+select ('2026-01-01'::date - current_date);
 
 -- 6 --
-
-SELECT ((DATE_TRUNC('year', lab_date)::date + INTERVAL '1 year')::date - lab_date) AS days_left
-FROM (SELECT DATE '2025-09-11' AS lab_date) t;
+select distinct 
+to_chat(ord_date, 'DD') as day,
+to_chat(ord_date, 'MM') as month,
+to_chat(ord_date, 'YY') as year
+from ord
 
 -- 7 --
-
 select name from sal
-where name ~* '^[^eyuioa]{2,}';
+where name ~* '^[bcdfghjklmnpqrstvwxyz]{2,}';
 
 -- 8 --
-
 select name from sal
 where name ~* '.k';
-
