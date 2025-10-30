a685ca3e6375f60d1a134080c1e0af57a5ac6390
Philipp Andruschenko <dxrk.rzn7txt@gmail.com>
2025-10-30 12:39:18 +0700

    DB: Lab 4 complete

---
+++ Курс 4/Семестр 1/DB/lab4.sql
@@ -0,0 +1,66 @@
+8.	Вывести из таблицы заказов уникальные номера продавцов и показатель активности каждого из них на основе количества заказов: «активный» — 4+ заказа, «умеренный» — 2-3 заказа, «неактивный» — 1 заказ.
+
+set search_path to my_schema;
+
+-- 1 --
+select * from sal
+where comm >= (select avg(comm) from sal);
+
+-- 2 --
+select cnum, sum(amt) from ord
+group by cnum
+having sum(amt) <= (
+  select sum(amt) from ord where cnum = 2003
+);
+
+-- 3 --
+select snum from ord
+where pnum = 1001 and cnum in (
+  select cnum from ord where pnum = 1005
+);
+
+-- 4 --
+select name from prod
+where pnum not in (
+  select pnum from ord where snum in (
+    select snum from sal where city = 'Moscow'
+  )
+);
+
+-- 5 --
+select name from cust
+where rating >= any (
+  select rating from cust where city = 'Moscow'
+);
+
+-- 6 --
+select name from sal
+where snum not in (
+  select snum from ord
+  where cnum in (
+    select cnum from cust
+    where city in (
+      select city from sal
+      where sal.snum = ord.snum
+    )
+  )
+);
+
+-- 7 --
+select snum, name, comm, city,
+    case
+        when comm < 0.12 then 'low'
+        when comm >= 0.12 and comm <= 0.13 then 'medium'
+        when comm > 0.13 then 'high'
+    end as "commission_level"
+from sal;
+
+-- 8 --
+select snum, count(*) as order_count,
+    case
+        when count(*) >= 4 then 'active'
+        when count(*) between 2 and 3 then 'moderate'
+        when count(*) = 1 then 'inactive'
+    end as activity_level
+from ord
+group by snum;
